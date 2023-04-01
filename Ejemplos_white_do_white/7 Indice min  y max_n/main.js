be6e8d644d36f43d58490d40684c0e2de78d6267
MartiPech <christianmartipech@gmail.com>
2023-04-01 17:12:59 -0300

    ejercicio 7

---
+++ Ejemplos_white_do_white/7 Indice min  y max_n/main.js
@@ -0,0 +1,49 @@
+//hacer un programa  . ingrese numeros . que el programa te diga cual es el primer menor y el segundo menor 
+
+let n = parseInt(prompt("ingrese numero"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let n = parseInt(prompt("Ingrese numero"));
+let indice = 1;
+let i_min=indice
+let i_max=indice
+let n_max = n;
+let n_min = n;
+
+while(n!==0){
+    n=parseInt(prompt("Ingrese numero"));
+    indice++
+
+    while(n_max<n){
+        n_max=n;
+        i_max=indice;
+    }
+
+    while(n_min>n && n!==0){
+        n_min=n;
+        i_min=indice
+    }
+}
+
+while(n == 0){
+    alert(" numero max "+ n_max+" indice del max "+ i_max+ " numero menor "+n_min+" indice del menor "+i_min );
+    if(n==0)break
+}
+*/
