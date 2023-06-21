83fa665d0269743a10b221752d199529d23c2b28
MartiPech <christianmartipech@gmail.com>
2023-06-21 18:34:44 -0300

    agregado eventos

---
+++ Ejemplos_Eventos/1 intro/main.js
@@ -0,0 +1,16 @@
+function mostrar(){
+    document.getElementById("seleccion").innerHTML= Date()
+}
+/*
+document ... lee todo el documento 
+getElementbyid ... busca/consigue el id (entiendo que en este caso para impirmirlo)
+innerHTML ... con esto le indico que devuelva algo al id 
+
+
+
+
+*/
+
+function mostrar2(){
+    document.getElementById("seleccion2").innerHTML=Date()
+}
